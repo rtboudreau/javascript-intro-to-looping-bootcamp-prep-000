41aaa21da74d29fe4e15a09cbc696dc194f6188b
Reid Boudreau <rtboudreau@gmail.com>
2018-12-18 19:53:37 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -25,9 +25,7 @@ function doWhileLoop(array) {
   }
   
   do {
-    console.log('array.length = ' + array.length + ' and i = ' + i);
-    array = array.slice(1)
-    incrementVariable();
+    array.pop
   } while (array.length > 0 && i < 5);
   
   return array
