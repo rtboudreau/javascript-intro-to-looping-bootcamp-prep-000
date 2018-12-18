4fd5195e8b3be7a3ac96330ec5664341e6443c0e
Reid Boudreau <rtboudreau@gmail.com>
2018-12-18 19:28:06 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,6 +2,6 @@ for (let i = 0; i < 25; i++){
   if (i === 1) {
     console.log('I am 1 strange loop.');
   } else{
-    return `I am ${1} strange loops.`;
+    console.log()I am ${1} strange loops.`;
   }
 }
